d89d0a6addffd89594493e39e8e6e85b0c8f0d97
ahmed isiaka <ahmeddavids6@gmail.com>
2024-10-28 11:37:55 +0100

    Added documentation for creating a user

--- API/routes/users.js
+++ API/routes/users.js
@@ -95,6 +95,32 @@ router.get('/:id', function (req, res) {
     });
 });
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a JSONPlaceholder user.
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The user ID.
+ *                       example: 0
+ *                     name:
+ *                       type: string
+ *                       description: The user's name.
+ *                       example: Leanne Graham
+*/
+
 router.post('/', function (req, res) {
   const url = baseURL;
 
