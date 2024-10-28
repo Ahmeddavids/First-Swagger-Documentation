9f94bffddf200b6738dd432f5d12e1b7d27ebab0
ahmed isiaka <ahmeddavids6@gmail.com>
2024-10-28 11:36:21 +0100

    Added documentation for get one user

--- API/routes/users.js
+++ API/routes/users.js
@@ -48,6 +48,39 @@ router.get('/', function (req, res) {
 
 });
 
+    /**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Retrieve a single JSONPlaceholder user.
+ *     description: Retrieve a single JSONPlaceholder user. Can be used to populate a user profile when prototyping or testing an API.
+ *     parameters:
+ *       -  in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: string
+ *     responses:
+ *       200:
+ *         description: A single user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The user ID.
+ *                       example: 0
+ *                     name:
+ *                       type: string
+ *                       description: The user's name.
+ *                       example: Leanne Graham
+*/
+
 router.get('/:id', function (req, res) {
   const userID = req.params.id || '';
   const url = `${baseURL}/${userID}`;
